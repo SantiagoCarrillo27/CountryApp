40c4497359f8708fa324bc3f3ce52cf208124e12
Analista Desarrollo1 <analista.desarrollo1@EXCELCREDIT.DOM>
2024-02-06 13:06:36 -0500

    Prueba de rutas github pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -35,9 +35,11 @@ const routes : Routes = [
 
 @NgModule({
 
-  imports: [RouterModule.forRoot(routes, {useHash:true})],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+// {useHash:true}
 
 export class AppRoutingModule{
 
